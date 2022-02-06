a3afecdcd6ef4019bd00fe70db9060dea00f2d97
Enos Vazquez <enosvazquez@gmail.com>
2022-02-06 10:29:01 -0800

    tools update

--- tools.py
+++ tools.py
@@ -4,6 +4,6 @@ from itertools import product
 a = [1, 2]
 b = [3, 4]
 prod = product(a, b)
-print(list(prod)) # This will combine all possibilies of those two variables
+print(list(prod)) # This output the combinations of 'a' and 'b'
 prod = product(a, b, repeat=2) #This will repeat the product 2x
 print(list(prod))
